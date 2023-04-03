d7c11aa8936bbb2d5d1281e4004824449e7af6be
oussemasghaier <oussema.sghaier@etudiant-enit.utm.tn>
2023-04-03 12:39:02 +0100

    simple birthday Card

---
+++ udemy_flutter/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFFD2BCD5),
+        body: Center(
+          child: Image(
+            image: AssetImage("images/birthdayCard.png"),
+          ),
+        ),
+      ),
+    );
+  }
+}
